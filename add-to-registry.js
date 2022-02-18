d9a03d35d0408641ced8f937abab4ac575b8b5bb
lenaorobei <oorobei@adobe.com>
2022-02-18 13:39:33 -0600

    Change workflow

---
+++ add-to-registry.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+//
+// const registry = fs.readFileSync('registry.json');
+// const registryObject= JSON.parse(registry);
+//
+//
+//
+// registryObject.push('new/template');
+//
+// const newData = JSON.stringify(registryObject);
+//
+//
+// fs.writeFile('registry.json', newData, err => {
+//     if(err) throw err;
+//     console.log("New template was added");
+// });
