68dc80858700e8c182060eb7e203064d8afecbae
lenaorobei <oorobei@adobe.com>
2022-03-28 14:24:12 -0500

    Update workflow

--- add-to-registry.js
+++ add-to-registry.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const myArgs = process.argv.slice(2);
 const body = myArgs[0];
-const pluginName = extractTemplateNameFromIssue(body);
+
+console.log(body);
+
+/*
 const registry = fs.readFileSync('registry.json');
 const registryObject= JSON.parse(registry);
 
@@ -11,29 +14,4 @@ fs.writeFile('registry.json', newData, err => {
     if(err) throw err;
     console.log('New template was added', newData);
 });
-
-/**
- * Extracting template name from the issue body.
- *
- * @param issueBody
- * @returns {string}
- */
-function extractTemplateNameFromIssue(issueBody) {
-    const matches = issueBody.split('\n')
-        .map((line) => {
-            const match = line ? line.match(/(https?:\/\/.[^ ]*)/gi) : null
-            if (match) {
-                return match.find((x) => x.includes('npmjs.com/package'));
-            }
-        })
-        .filter((m) => m)
-        .map((x) => {
-            return x.split('/').splice(4).join('/').replace(/[^a-zA-Z0-9@\\/-]/g, '');
-        });
-
-    if (matches.length) {
-        return matches[0];
-    } else {
-        throw new Error('Cannot determine template name.');
-    }
-}
+*/
